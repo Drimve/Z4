faaeb9239b3fbed36046a725dc2cee41f0a3caa8
Drimve <86664972+Drimve@users.noreply.github.com>
2021-11-08 14:07:57 +0500

    Add files via upload

---
+++ Лаба4.1-2 -/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        AppDbContext db;
+        public HomeController(AppDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Лаба4.1-2 -/Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        public DateTime YearOfBirth { get; set; }
+    }
+}
