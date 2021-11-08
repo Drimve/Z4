2f6a839a4ab31231559f7e67375009e9e3a6509c
Drimve <86664972+Drimve@users.noreply.github.com>
2021-11-08 14:06:41 +0500

    Add files via upload

---
+++ Лаба4.3/Persistent/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shop;
+
+namespace Shop
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+    }
+}
---
+++ Лаба4.3/SampleData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class SampleData
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                context.Users.AddRange(
+                    new User
+                    {
+                        Surname = "Иванов",
+                        Name = "Иван",
+                        Gender = 'м',
+                        YearOfBirth = new DateTime(2000, 11, 10)
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
